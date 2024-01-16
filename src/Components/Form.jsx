4013d743538b1cea2ac23470a9f9a5101393f292
AllSparkTech100 <ikangenyinabasiono@gmail.com>
2024-01-16 23:41:51 +0100

    updates

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -4,7 +4,7 @@ import { Col, Row, Container, Form, Button } from "react-bootstrap";
 function FormComp() {
   return (
     <Container className="bg-image conta px-0 py-5 p-md-5" fluid>
-      <div className="form-carrier w-50 h-100">
+      <div className="form-carrier h-100">
         <div className="head">
           <Col className="text-center">
             <h4>Book a Table</h4>
@@ -14,33 +14,38 @@ function FormComp() {
         <Form className="p-3">
           <Row className="mb-3">
             <Form.Group as={Col} controlId="formGridName">
-              <Form.Label>Name</Form.Label>
-              <Form.Control type="name" placeholder="Your Name" />
+              <Form.Label className='fw-bold'>Name</Form.Label>
+              <Form.Control className='border-1 shadow-none 
+rounded-1' type="name" placeholder="Your Name" />
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Your Email" />
+              <Form.Label className='fw-bold'>Email</Form.Label>
+              <Form.Control className='border-1 shadow-none 
+rounded-1' type="email" placeholder="Your Email" />
             </Form.Group>
           </Row>
           <Row className="mb-3">
             <Form.Group as={Col} controlId="formGridPhone">
-              <Form.Label>Phone</Form.Label>
-              <Form.Control placeholder='+234-810-029-3828'/>
+              <Form.Label className='fw-bold'>Phone</Form.Label>
+              <Form.Control className='border-1 shadow-none 
+rounded-1' placeholder='+234-810-029-3828'/>
             </Form.Group>
             <Form.Group as={Col} controlId="formGridPhone">
-              <Form.Label>Phone</Form.Label>
-              <Form.Control placeholder='+234-810-029-3828'/>
+              <Form.Label className='fw-bold'>Phone</Form.Label>
+              <Form.Control className='border-1 shadow-none 
+rounded-1' placeholder='+234-810-029-3828'/>
             </Form.Group>           
           </Row>
 
           <Row className='mb-3'>
           <Form.Group as={Col} controlId="formGridPhone">
-              <Form.Label>Time</Form.Label>
-              <Form.Control placeholder='e.g: 6:00pm'/>
+              <Form.Label className='fw-bold'>Time</Form.Label>
+              <Form.Control className='border-1 shadow-none 
+rounded-1' placeholder='e.g: 6:00pm'/>
             </Form.Group>
             <Form.Group as={Col} controlId="formGridState">
-              <Form.Label>Number Of Person(s)</Form.Label>
+              <Form.Label className='fw-bold'>Person(s)</Form.Label>
               <Form.Select defaultValue="Choose...">
                 <option>Choose...</option>
                 <option>1</option>
@@ -54,7 +59,7 @@ function FormComp() {
           </Row>
           
            
-          <Button variant="primary" className="float-end text-center rounded-1 " type="submit">
+          <Button variant="primary" className="text-center border-0 mt-3 rounded-1 " type="submit">
            Make a Reseravation
           </Button>
         </Form>
