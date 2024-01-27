c51d4078ccc601e1aac8b5b7c9d092657fa8db63
AllSparkTech100 <ikangenyinabasiono@gmail.com>
2024-01-27 11:46:18 +0100

    up

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,31 +1,36 @@
 import { Container, Col, Row, Form, Button } from "react-bootstrap";
-import './button.scss'
-import { Link } from "react-router-dom";
+import "./button.scss";
 import {
   FaTwitter,
   FaGooglePlusG,
   FaFacebookF,
   FaInstagram,
 } from "react-icons/fa";
-  
+
 function Footer() {
   return (
     <div>
-      <Container className="bg-black mt-5" fluid>
+      <Container className="mt-5 text-muted footer_carrier" fluid>
         <Container className="mt-5 p-5 text-white">
           <Row>
             <Col md={3} sm={6}>
-              <h4 className="fs-1 fw-bold text-white">Feliciano</h4>
-              <h4>Working Hours</h4>
-              <h4>Working Hours</h4>
-              <h4>Working Hours</h4>
-              <h4>Working Hours</h4>
-              <h4>Working Hours</h4>
-              <h4>Working Hours</h4>
-              <div className="socials">
-                  <FaTwitter size={20} /> <FaFacebookF size={20} />
-                  <FaGooglePlusG size={20} /> <FaInstagram size={20} />
+              <h4 className="fs-1 mb-4 fw-bold text-white">Feliciano</h4>
+              <div className="mb-4 text-muted fw-medium">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Praesentium blanditiis qui saepe omnis adipisci libero soluta
+                  odio. Rem modi delectus, maiores illum cum in!
+              </div>
+              <div className="circle_socials">
+                <div className="circle_content">
+                  <FaTwitter size={20} />
                 </div>
+                <div className="circle_content">
+                  <FaFacebookF size={20} />
+                </div>
+                <div className="circle_content">
+                  <FaInstagram size={20} />
+                </div>
+              </div>
             </Col>
             <Col md={3} sm={6}>
               <h4>Working Hours</h4>
@@ -57,9 +62,12 @@ function Footer() {
                   />
                 </Form.Group>
                 <div className="sub_btn mt-3">
-                <Button className="border-0 rounded-0 btn_sub shadow-none" type="submit">
-                Subscribe
-              </Button>
+                  <Button
+                    className="border-0 rounded-0 btn_sub shadow-none"
+                    type="submit"
+                  >
+                    Subscribe
+                  </Button>
                 </div>
               </div>
             </Col>
